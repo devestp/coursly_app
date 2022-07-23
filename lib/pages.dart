a7d434000ddea61174ad33bbbe41a5bc095505e3
sina <im.devestp@gmail.com>
2022-07-23 17:41:07 +0430

    feat: add transition to /home page

--- lib/pages.dart
+++ lib/pages.dart
@@ -4,5 +4,10 @@ import 'screens/screens.dart';
 
 final pages = <GetPage>[
   GetPage(name: '/intro', page: () => const IntroScreen()),
-  GetPage(name: '/home', page: () => const HomeScreen()),
+  GetPage(
+    name: '/home',
+    page: () => const HomeScreen(),
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
 ];
